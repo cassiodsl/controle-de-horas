4e815bf3a3f61430df8e646d5b9152b3d36cfe1a
Fabio Fiuza <fabiomindua@gmail.com>
2014-06-28 21:15:02 -0300

    Alteração do Alex

--- src/br/com/jovens/controledehoras/MainActivity.java
+++ src/br/com/jovens/controledehoras/MainActivity.java
@@ -2,8 +2,6 @@ package br.com.jovens.controledehoras;
 
 import android.app.Activity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
 
 public class MainActivity extends Activity {
 
